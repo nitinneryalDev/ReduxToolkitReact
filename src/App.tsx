cb58c8ee36fb45e7314dfd51de9ccd8e2075e7e2
nitinneryalDev <nitinneryal@gmaile.com>
2024-11-12 21:16:32 +0530

    Typescript and redux toolkit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Provider } from "react-redux";
+import store from "./store/store";
+import { Container, Typography } from "@mui/material";
+import AddHabitForm from "./components/add-habit-form";
+import HabitList from "./components/habit-list";
+import HabitStats from "./components/habits-stats";
+
+function App() {
+  return (
+    <>
+      <Provider store={store}>
+        <Container>
+          <Typography component="h1" variant="h2" align="center">
+            Habit Tracker
+          </Typography>
+          <AddHabitForm/>
+          <HabitList/>
+          <HabitStats/>
+        </Container>
+       
+      </Provider>
+    </>
+  );
+}
+
+export default App;
